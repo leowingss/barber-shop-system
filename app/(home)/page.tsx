4195a78eb96f2d972f41fa33959ba867a092c38e
Leonardo <leondani853@gmail.com>
2024-02-02 12:42:16 -0300

    feat: ensure confirmed bookings are shown on homepage.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,43 +1,77 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
-import { ptBR } from "date-fns/locale/pt-BR";
+import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
-import { Barbershop } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { Barbershop, Booking } from "@prisma/client";
 
 export default async function Home() {
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div>
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Leonardo!</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
         <p className="capitalize text-sm">
-          {format(new Date(), "EEEE', 'dd 'de' MMMM'", {
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
         </p>
       </div>
+
       <div className="px-5 mt-6">
         <Search />
       </div>
-      {/* <div className="px-5 mt-6">
-        <h2 className="text-sm uppercase mb-3 text-gray-400 font-bold">
-          Agendamentos
-        </h2>
-        <BookingItem />
-      </div> */}
 
       <div className="mt-6">
-        <h2 className="px-5 text-sm uppercase mb-3 text-gray-400 font-bold">
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+              Agendamentos
+            </h2>
+            <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+              {confirmedBookings.map((booking: Booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Recomendados
         </h2>
 
-        <div className="flex gap-4 px-5 overflow-x-auto [&::webkit-scrollbar]:hidden">
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop: Barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
@@ -45,11 +79,11 @@ export default async function Home() {
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-sm uppercase mb-3 text-gray-400 font-bold">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Populares
         </h2>
 
-        <div className="px-5 flex gap-4 overflow-x-auto [&::webkit-scrollbar]:hidden">
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop: Barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,64 +1,82 @@
 import { getServerSession } from "next-auth";
 import Header from "../_components/header";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import { signIn } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
 import { Booking } from "@prisma/client";
-import { isFuture, isPast } from "date-fns";
 
-const BookingPage = async () => {
+const BookingsPage = async () => {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
     return redirect("/");
   }
 
-  const bookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-    },
-    include: {
-      service: true,
-      barbershop: true,
-    },
-  });
-
-  const confirmedBookings = bookings.filter((booking: Booking) =>
-    isFuture(booking.date)
-  );
-  const finishedBooking = bookings.filter((booking: Booking) =>
-    isPast(booking.date)
-  );
+  const [confirmedBookings, finishedBookings] = await Promise.all([
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          lt: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+  ]);
 
   return (
     <>
       <Header />
+
       <div className="px-5 py-6">
-        <h1 className="text-xl font-bold">Agendamentos</h1>
-        <h2 className="text-gray-400 mt-6 mb-3 uppercase text-sm font-bold">
-          Confirmados
-        </h2>
+        <h1 className="text-xl font-bold mb-6">Agendamentos</h1>
 
-        <div className="flex flex-col gap-3">
-          {confirmedBookings.map((booking: Booking) => (
-            <BookingItem booking={booking} key={booking.id} />
-          ))}
-        </div>
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">
+              Confirmados
+            </h2>
 
-        <h2 className="text-gray-400 mt-6 mb-3 uppercase text-sm font-bold">
-          Finalizados
-        </h2>
+            <div className="flex flex-col gap-3">
+              {confirmedBookings.map((booking: Booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
 
-        <div className="flex flex-col gap-3">
-          {finishedBooking.map((booking: Booking) => (
-            <BookingItem booking={booking} key={booking.id} />
-          ))}
-        </div>
+        {finishedBookings.length > 0 && (
+          <>
+            <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
+              Finalizados
+            </h2>
+
+            <div className="flex flex-col gap-3">
+              {finishedBookings.map((booking: Booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </>
   );
 };
 
-export default BookingPage;
+export default BookingsPage;
